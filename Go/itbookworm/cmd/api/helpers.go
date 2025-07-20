c5e221b9fa50854330cb816abe0140b3df1343db
Envo1d <79973093+Envo1d@users.noreply.github.com>
2025-07-20 23:50:53 +0300

    feat: golang base http server

---
+++ Go/itbookworm/cmd/api/helpers.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func (app *application) readIDParam(r *http.Request) (int64, error) {
+	param := chi.URLParam(r, "id")
+
+	id, err := strconv.ParseInt(param, 10, 64)
+	if err != nil || id < 1 {
+		return 0, errors.New("invalid parameter")
+	}
+
+	return id, nil
+}
---
+++ Go/itbookworm/go.mod
@@ -0,0 +1,5 @@
+module github.com/Envo1d/itbookworm
+
+go 1.24.5
+
+require github.com/go-chi/chi/v5 v5.2.2 // direct
